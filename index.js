91c03a8c2432f13001f2a9b0bdea537fbe18c480
uchenna <uchennaongit@gmail.com>
2022-08-17 01:18:42 +0100

    Modify index.js file

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require("express")
-// const dotenv =
+const app = express()
+
+const cors = require('cors')
+const port = process.env.PORT||8000
+
 require('dotenv').config()
 const mongoose = require("mongoose")
 const authRoute = require('./routes/auth')
@@ -7,16 +11,13 @@ const userRoute = require('./routes/user')
 const productRoute = require('./routes/product')
 const cartRoute = require('./routes/cart')
 const orderRoute = require('./routes/order')
-const cors = require('cors')
 
-// dotenv.config()
 
 mongoose
 .connect(process.env.MONGO_URL)
 .then(() => console.log("DB connected"))
 .catch(err => console.log(err))
 
-const app = express()
 
 app.use(cors())
 app.use(express.json())
@@ -30,6 +31,5 @@ app.use('/api/products', productRoute)
 app.use('/api/carts', cartRoute)
 app.use('/api/orders', orderRoute)
 
-const port = process.env.PORT ||8000
 
 app.listen(port, () => console.log(`Server established on port ${port}`))
